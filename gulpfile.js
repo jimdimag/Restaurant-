63430c3842b54c2827258479df7c45119eade09a
James DiMaggio <jimdimag@gmail.com>
2014-09-19 14:47:59 -0400

    Added json task

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var jsSources = [
 ];
 var sassSources = ['components/sass/style.scss'];
 var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
     
 gulp.task('coffee', function() {
    gulp.src(coffeeSources)
@@ -47,6 +48,7 @@ gulp.task('watch', function(){
    gulp.watch(jsSources, ['js']);
    gulp.watch('components/sass/*.scss', ['compass']);
    gulp.watch(htmlSources, ['html']);
+   gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect',function() {
@@ -59,5 +61,9 @@ gulp.task('html', function() {
    gulp.src(htmlSources)
    .pipe(connect.reload())
 });
+gulp.task('json', function() {
+   gulp.src(jsonSources)
+   .pipe(connect.reload())
+});
 
-gulp.task('default',['html','coffee','js','compass','connect','watch']);
+gulp.task('default',['html','json','coffee','js','compass','connect','watch']);
